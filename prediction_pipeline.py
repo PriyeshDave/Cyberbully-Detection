9fb1d9974dcc63f4382af079776677c9b2eb5b95
Priyesh <priyeshdave@icloud.com>
2022-04-23 18:22:14 +0530

    fixed one hot encoding error

--- prediction_pipeline.py
+++ prediction_pipeline.py
@@ -1,15 +1,18 @@
+import pandas as pd
 import re
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 import tensorflow
 import joblib
-from tensorflow.keras.preprocessing.text import one_hot
+#from tensorflow.keras.preprocessing.text import one_hot
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 sw = stopwords.words('english')
 lm = joblib.load('./Models/Lemmatizer.pkl')
-model = tensorflow.keras.models.load_model('./LSTM/Callbacks 3/model.02-0.42.h5')
-mapper = {False: 'Not Toxic', True:'Toxic'}
+model = tensorflow.keras.models.load_model('./LSTM/Callbacks/model.02-0.42.h5')
+one_hot_df = pd.read_csv('./Datasets/One Hot Encoded Data.csv').set_index('Word')
+
+mapper = {0 : 'Not Toxic', 1 : 'Toxic'}
 
 voc_size = 16000
 sent_length = 25
@@ -50,19 +53,25 @@ def cleanData(sentence):
   return sentence.strip()
 
 def word_embedding(sentence):
+  sentence = sentence.split(' ')
   # One hot representation of words
-  onehot_repr = [one_hot(sentence,voc_size)] 
+  onehot_repr = [one_hot_df.loc[word][0] for word in sentence]
   # Embedding the sentences
-  embedded_docs = pad_sequences(onehot_repr,padding='pre',maxlen=sent_length)
+  embedded_docs = pad_sequences([onehot_repr],padding='pre',maxlen=sent_length)
   return embedded_docs
 
 def get_predictions(sentence):
+  print('Sentence Length: ', len(sentence))
   sentence = cleanData(sentence)
+  print('Sentence Length: ', len(sentence))
+  print(sentence)
   data_lstm = word_embedding(sentence)
-  prediction = model.predict(data_lstm)
-  prediction = (prediction >= 0.5)
+  print('Embeded Docs: ', data_lstm)
+  predicted_probability = model.predict(data_lstm)[0][0]
+  print('Predicted Probability: ', predicted_probability)
+  prediction = (predicted_probability >= 0.5)
 
-  return mapper[prediction[0][0]]
+  return (mapper[int(prediction)], predicted_probability)
   
 
 
